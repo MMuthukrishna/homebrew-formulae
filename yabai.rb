e5562fa6a8b296c07992fc0fa1604467c0ddce6d
MMuthukrishna <muthukrishna.m@instamojo.com>
2020-04-18 12:22:11 +0530

    Add yabai.rb

---
+++ yabai.rb
@@ -0,0 +1,69 @@
+
+class Yabai < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning."
+  homepage "https://github.com/MMuthukrishna/yabai"
+  url "https://github.com/MMuthukrishna/yabai/releases/download/v2.4.3/yabai-v2.4.3.tar.gz"
+  sha256 "eee99910dc76f66c3db5938d81e3730d548ce2c5d3fcb9d469e2ab8cd2f0704f"
+  head "https://github.com/MMuthukrishna/yabai.git"
+
+  depends_on :macos => :high_sierra
+
+  def install
+    (var/"log/yabai").mkpath
+    man.mkpath
+
+    if build.head?
+      ENV.O2
+      system "make", "install"
+    end
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+    Logs will be found in
+      #{var}/log/yabai/yabai.[out|err].log
+    EOS
+  end
+
+  plist_options :manual => "yabai"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/yabai</string>
+      </array>
+      <key>EnvironmentVariables</key>
+      <dict>
+        <key>PATH</key>
+        <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+      </dict>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/yabai/yabai.out.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/yabai/yabai.err.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    assert_match "yabai-v#{version}", shell_output("#{bin}/yabai --version")
+  end
+end
